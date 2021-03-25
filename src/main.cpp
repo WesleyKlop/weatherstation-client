8e3a00af670adf4ecf2c8f6b052967a8f94a0fdf
Wesley Klop <w.klop@inthere.nl>
2021-03-25 21:07:46 +0100

    Connect to wifi

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include <Arduino.h>
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include "secrets.h"
 
 #define DHT_PIN D2
-
 
 DHT dht = DHT(DHT_PIN, DHT22);
 
@@ -11,9 +13,18 @@ void setup() {
     Serial.begin(9600);
 
     dht.begin();
+    WiFi.begin(WIFI_SSID, WIFI_PASSWD);
+
+    int tries = 0;
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(1000);
+        Serial.printf("Connecting... %d\t", ++tries);
+    }
+
+    Serial.printf("Connected to %s with IP Address: %s", WiFi.SSID().c_str(), WiFi.localIP().toString().c_str());
 }
 
-const int MEASURE_INTERVAL = 1000 * 60 * 5;
+#define MEASURE_INTERVAL (1000 * 60 * 5);
 //const int MEASURE_INTERVAL = 5000;
 
 void loop() {
