4c1b2170f3199ce41a76e45a51484dd6f28aae6f
Wesley Klop <w.klop@inthere.nl>
2021-03-25 20:56:47 +0100

    Implement fetching data from sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,41 @@
 #include <Arduino.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+
+#define DHT_PIN D2
+
+
+DHT dht = DHT(DHT_PIN, DHT22);
 
 void setup() {
-// write your initialization code here
+    Serial.begin(9600);
+
+    dht.begin();
 }
 
+const int MEASURE_INTERVAL = 1000 * 60 * 5;
+//const int MEASURE_INTERVAL = 5000;
+
 void loop() {
-// write your code here
+    if (millis() % MEASURE_INTERVAL != 0) {
+        return;
+    }
+
+    // Read the humidity and temperature in %:
+    float humidity = dht.readHumidity();
+    float temperature = dht.readTemperature();
+    float heat_index = dht.computeHeatIndex(temperature, humidity, false);
+
+    Serial.print("Humidity: ");
+    Serial.print(humidity);
+    Serial.println("%");
+    Serial.print("Temperature: ");
+    Serial.print(temperature);
+    Serial.println("°C");
+    Serial.print("Feels like: ");
+    Serial.print(heat_index);
+    Serial.println("°C");
+    Serial.println("Read finished...");
+
+    delay(1);
 }
