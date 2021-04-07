961e50a62b2715fbfc3f7df2dc1143d0d08d26ff
Wesley Klop <w.klop@inthere.nl>
2021-04-07 16:05:43 +0200

    start working on mq135

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,15 @@
 #include <DHT.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
+#include <MQUnifiedsensor.h>
 #include "secrets.h"
 
 #define DHT_PIN D2
+#define MQ_PIN A0
 
 DHT dht = DHT(DHT_PIN, DHT22);
+MQUnifiedsensor MQ135("Arduino NANO", 5, 10, MQ_PIN, "MQ-135");
+
 HTTPClient http;
 WiFiClientSecure wifiClient;
 
@@ -28,6 +32,11 @@ void setup() {
     Serial.begin(9600);
 
     dht.begin();
+    MQ135.setRegressionMethod(1);
+    MQ135.init();
+    MQ135.setA(110.47);
+    MQ135.setB(-2.862); // Configurate the ecuation values to get CO2 concentration
+
     WiFi.begin(WIFI_SSID, WIFI_PASSWD);
     wifiClient.setInsecure();
 
@@ -43,9 +52,12 @@ void setup() {
 const int MEASURE_INTERVAL = (1000 * 60 * 5);
 
 measurement_t get_latest_measurement() {
+    MQ135.update();
+
     measurement_t measurement{};
     measurement.humidity = dht.readHumidity();
     measurement.temperature = dht.readTemperature();
+    measurement.carbon_dioxide = MQ135.readSensor();
 
     return measurement;
 }
